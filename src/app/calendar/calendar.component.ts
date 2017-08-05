8c37761946f326692d1bf78e1c985a4770651779
Gabriel <gabriellopezdv@gmail.com>
2017-08-05 00:45:31 -0500

    Select day from the calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { CalendarEvent, CalendarMonthViewDay } from 'angular-calendar';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class CalendarComponent implements OnInit {
   viewDate= new Date();
+
+  view= 'month';
+  selectedDay: CalendarMonthViewDay;
+  clickedDate= this.viewDate;
+  locale = 'es';
+
+
+  events: CalendarEvent[] = [];
+
+  dayClicked(day: CalendarMonthViewDay): void {
+    if (this.selectedDay) {
+      delete this.selectedDay.cssClass;
+    }
+    day.cssClass = 'cal-day-selected';
+    this.selectedDay = day;
+    this.clickedDate = day.date;
+  }
+
+  beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
+    body.forEach(day => {
+      if (
+        this.selectedDay &&
+        day.date.getTime() === this.selectedDay.date.getTime()
+      ) {
+        day.cssClass = 'cal-day-selected';
+        this.selectedDay = day;
+      }
+    });
+  }
 
   constructor() { }
 
