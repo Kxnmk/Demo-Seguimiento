f1889068e25bbec9ae49309e603d9b4743eb7ae3
Gabriel <gabriellopezdv@gmail.com>
2017-08-05 12:32:59 -0500

    Events Added

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { CalendarEvent, CalendarMonthViewDay } from 'angular-calendar';
 
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -15,9 +30,39 @@ export class CalendarComponent implements OnInit {
   selectedDay: CalendarMonthViewDay;
   clickedDate= this.viewDate;
   locale = 'es';
-
-
-  events: CalendarEvent[] = [];
+  // Date Format 2017-08-16T10:00:00
+  events: CalendarEvent[] = [
+    {
+      title: 'Evento 1',
+      color: colors.blue,
+      start: new Date("2017-08-21T15:00:00")
+    },
+    {
+      title: 'Evento 2',
+      color: colors.blue,
+      start: new Date("2017-08-16T10:00:00")
+    },
+    {
+      title: 'Evento 2.1',
+      color: colors.blue,
+      start: new Date("2017-08-16T15:00:00")
+    },
+    {
+      title: 'Evento 2.2',
+      color: colors.blue,
+      start: new Date("2017-08-16T09:00:00")
+    },
+    {
+      title: 'Evento 3',
+      color: colors.blue,
+      start: new Date("2017-08-4T17:00:00")
+    },
+    {
+      title: 'Evento 4',
+      color: colors.blue,
+      start: new Date("2017-08-10T09:00:00")
+    }
+  ];
 
   dayClicked(day: CalendarMonthViewDay): void {
     if (this.selectedDay) {
@@ -38,6 +83,10 @@ export class CalendarComponent implements OnInit {
         this.selectedDay = day;
       }
     });
+  }
+
+  eventClicked({ event }: { event: CalendarEvent }): void {
+    console.log('Event clicked', event);
   }
 
   constructor() { }
